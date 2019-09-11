9b251adb04673bdee769987859043797d3ce9b10
ubuntu-1 <mindnerves>
2019-09-11 15:49:26 +0530

    lgin/register pages updated

--- src/js/components/Login/LoginForm.js
+++ src/js/components/Login/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Form, FormGroup, Label, Input, Button, Row, Col, Breadcrumb, BreadcrumbItem  } from 'reactstrap';
 import { FormattedMessage } from 'react-intl';
+import { AvForm, AvField } from 'availity-reactstrap-validation';
 import '../../../styles/Login/Login.css';
 
 class LoginForm extends Component {
@@ -22,14 +23,19 @@ class LoginForm extends Component {
                 <Row>
                     <Col md="4"></Col>
                     <Col md="4">
-                    <Form>
+                    <AvForm>
                         <Row>
                             <Col md="1"></Col>
                             <Col md="10" className="text-align-left pt-4 pl-4">
                                 <FormGroup>
                                     <Label for="loginEmailLabel"><b><FormattedMessage id="login.email" defaultMessage="Email address" /></b></Label>
 
-                                    <Input type="email" name="email" value={this.state.email} size="40" maxLength="100" onChange={this.onChangeCredintials}/>
+                                    <AvField type="email" name="email" value={this.state.email} size="40" maxLength="100" onChange={this.onChangeCredintials}
+                                    validate={{
+                                              required: { value: true, errorMessage: <FormattedMessage id="register.validation.email.empty" defaultMessage="Email address cannot be empty" /> },
+                                              pattern: { value: /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/, errorMessage: <FormattedMessage id="register.validation.email.invalid" defaultMessage="Email address is not valid" /> }
+                                            }}
+                                    />
                                 </FormGroup>
                             </Col>
                             <Col md="1"></Col>
@@ -40,7 +46,11 @@ class LoginForm extends Component {
                             <Col md="10" className="text-align-left pl-4">
                                 <FormGroup>
                                     <Label for="loginPasswordLabel"><b><FormattedMessage id="login.password" defaultMessage="Password" /></b></Label>
-                                    <Input type="password" name="password" size="40" maxLength="100" value={this.state.password} onChange={this.onChangeCredintials}/>
+                                    <AvField type="password" name="password" size="40" maxLength="100" value={this.state.password} onChange={this.onChangeCredintials}
+                                        validate = {{
+                                            required:{value: true, errorMessage: <FormattedMessage id="register.validation.password.empty" defaultMessage="Password cannot be empty" /> }
+                                        }}
+                                    />
                                 </FormGroup>
                             </Col>
                             <Col md="1"></Col>
@@ -71,7 +81,7 @@ class LoginForm extends Component {
                             </Col>
                             <Col md="1"></Col>
                         </Row>
-                    </Form>
+                    </AvForm>
                     </Col>
                     <Col md="4"></Col>
                 </Row>
